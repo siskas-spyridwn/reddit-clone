2621f6195fb87022ee8f82983717fc6170bbe96a
Spiros Siskas <spiros.siskas@gmail.com>
2020-07-23 22:33:00 +0300

    New UserController with CRUD operations

---
+++ src/main/java/com/redditclone/controller/UserController.java
@@ -0,0 +1,41 @@
+package com.redditclone.controller;
+
+import com.redditclone.model.User;
+import org.springframework.web.bind.annotation.*;
+
+// User CRUD
+@RestController
+public class UserController {
+
+    // Create user
+    @PostMapping("/user")
+    public User userPost(@RequestBody User user) {
+        //save user to db
+        return user;
+    }
+
+    // Read User
+    @GetMapping("/user/{userId}")
+    public User userWorld(@PathVariable(value = "userId") Long userId) {
+
+        // Get from db the user with id 'userId'
+        User user = new User();
+        user.setUsername("Chris");
+        return user;
+    }
+
+    // Update user
+    @PutMapping("/user/{userId}")
+    public User userPut(@PathVariable(value = "userId") Long userId, @RequestBody User user) {
+        // update user
+        return user;
+    }
+
+    // Delete user
+    @DeleteMapping("/user/{userId}")
+    public User userDelete(@PathVariable(value = "userId") Long userId) {
+        return null;
+    }
+
+
+}
---
+++ src/main/java/com/redditclone/model/User.java
@@ -0,0 +1,50 @@
+package com.redditclone.model;
+
+public class User {
+
+    private Long userId;
+    private String username;
+    private String email;
+    private String password;
+    private String dateOfBirth;
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+}
